1df204c319d6a55a8c6b5c7ed2252de27776a1cb
JCM <jacama2009@gmail.com>
2021-08-07 17:11:35 +0200

    [vulkan_layer] Make shader compile available to plugins

--- vulkan_layer/include/shaders.hpp
+++ vulkan_layer/include/shaders.hpp
@@ -5,3 +5,8 @@
 
 using CheekyLayer::VkHandle;
 extern std::map<VkHandle, VkHandle> customShaderHandles;
+
+#ifdef USE_GLSLANG
+#include <ShaderLang.h>
+extern std::tuple<bool, std::string> compileShader(EShLanguage stage, std::string glslCode, std::vector<unsigned int>& shaderCode);
+#endif
