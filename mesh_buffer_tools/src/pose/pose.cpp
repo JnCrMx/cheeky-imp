8c42c25d19d93b47d2ef7805e2907d99b7cddcb6
JCM <jacama2009@gmail.com>
2022-05-13 23:15:40 +0200

    [mesh_buffer_tools] More options for poses

--- mesh_buffer_tools/src/pose/pose.cpp
+++ mesh_buffer_tools/src/pose/pose.cpp
@@ -4,6 +4,8 @@
 #include <fstream>
 #include <vector>
 #include <map>
+#include <iostream>
+#include <iomanip>
 
 #include <boost/interprocess/file_mapping.hpp>
 #include <boost/interprocess/mapped_region.hpp>
@@ -16,7 +18,13 @@
 struct pose_part
 {
 	std::string name;
+	std::string file;
 	int offset;
+};
+
+struct options
+{
+	bool transpose = false;
 };
 
 int main(int argc, char *argv[])
@@ -26,30 +34,49 @@ int main(int argc, char *argv[])
 	std::string input_path = argv[1];
 	std::string output_path = argv[2];
 
-	std::vector<pose_part> parts(argc-3);
-	for(int i=0; i<argc-3; i++)
+	options my_options = {};
+	int partsOffset;
+	for(partsOffset = 3; std::string(argv[partsOffset]).starts_with("--"); partsOffset++)
 	{
-		std::string line = argv[i+3];
+		std::string option = std::string(argv[partsOffset]).substr(2);
+		std::cout << "Enabling option " << option << ".\n";
+
+		if(option == "transpose")
+			my_options.transpose = true;
+		else
+			std::cerr << "Unknown option: --" << option << std::endl;
+	}
+
+	std::vector<pose_part> parts(argc-partsOffset);
+	for(int i=0; i<argc-partsOffset; i++)
+	{
+		std::string line = argv[i+partsOffset];
+		std::cout << "Using part " << line << ".\n";
 
 		std::istringstream is_line(line);
 
 		std::string key;
-		std::string value;
-		if(std::getline(is_line, key, '=') && std::getline(is_line, value, '='))
+		std::string file;
+		std::string offset;
+		if(std::getline(is_line, key, '=') && std::getline(is_line, file, ':') && std::getline(is_line, offset))
 		{
-			int val = std::stoi(value, nullptr, 0);
-			parts[i] = {key, val};
+			int o = std::stoi(offset, nullptr, 0);
+			parts[i] = {key, file, o};
+		}
+		else
+		{
+			std::cerr << "Couldn't read part: " << line << std::endl;
 		}
 	}
-
-	boost::interprocess::file_mapping map(input_path.c_str(), boost::interprocess::read_only);
-	boost::interprocess::mapped_region region(map, boost::interprocess::read_only);
-	glm::mat3x4* matrices = (glm::mat3x4*) region.get_address();
 
 	std::map<std::string, std::map<std::string, std::array<float, 16>>> groupMap;
 
 	for(pose_part part : parts)
 	{
+		boost::interprocess::file_mapping map((input_path+"/"+part.file).c_str(), boost::interprocess::read_only);
+		boost::interprocess::mapped_region region(map, boost::interprocess::read_only);
+		glm::mat3x4* matrices = (glm::mat3x4*) region.get_address();
+
 		glm::mat3x4* base = matrices + part.offset;
 		for(int i = 0; i < 256; i++)
 		{
@@ -61,7 +88,8 @@ int main(int argc, char *argv[])
 			mat2[3][2] = mat2[2][3];
 			mat2[3][3] = 0.0;
 
-			mat2 = glm::transpose(mat2);
+			if(my_options.transpose)
+				mat2 = glm::transpose(mat2);
 
 			std::array<float, 16> floats;
 
@@ -74,5 +102,5 @@ int main(int argc, char *argv[])
 	}
 	nlohmann::json j = groupMap;
 	std::ofstream outJson(output_path);
-	outJson << j;
+	outJson << std::setw(4) << j;
 }
